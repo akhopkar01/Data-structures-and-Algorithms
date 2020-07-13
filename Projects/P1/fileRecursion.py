5e3df9208d6ab4656e4ee16a9b47f60119bedd8f
akhopkar01 <aadi0110@gmail.com>
2020-07-13 14:56:48 -0400

    Modified changes

--- Projects/P1/fileRecursion.py
+++ Projects/P1/fileRecursion.py
@@ -4,23 +4,28 @@ path_base = os.getcwd() + '/testdir'
 
 def find_files(path, suffix):
     if suffix == '':
-        return []
+        return
 
     if len(os.listdir(path)) == 0:
-        return []
+        return
 
     directory = os.listdir(path)
     files = []
     folders = []
     for elements in directory:
         if "."+suffix in elements:
-            files.append(elements)
+            files.append(path+"/"+elements)
         elif "." not in elements:
             folders.append(elements)
-
+    # print(folders)
     for folder in folders:
-        files.append(find_files(path+"/"+folder, suffix))
-
+        new_files = find_files(path+"/"+folder, suffix)
+        if new_files:
+            files.append(new_files[0])
     return files
 
 print(find_files(suffix='c', path=path_base))
+
+# print(find_files(suffix='h', path=path_base))
+#
+# print(find_files(suffix='gitkeep', path=path_base))
