ee724b8df7eb8d6b439692c408f02d12d75e856a
akhopkar01 <aadi0110@gmail.com>
2020-07-15 16:34:45 -0400

    Union Intersection of Linked List added

---
+++ Projects/P1/Union_Intersection.py
@@ -0,0 +1,105 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __repr__(self):
+        return str(self.value)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __str__(self):
+        cur_head = self.head
+        out_string = ""
+        while cur_head:
+            out_string += str(cur_head.value) + " -> "
+            cur_head = cur_head.next
+        return out_string
+
+
+    def append(self, value):
+
+        if self.head is None:
+            self.head = Node(value)
+            return
+
+        node = self.head
+        while node.next:
+            node = node.next
+
+        node.next = Node(value)
+
+    def size(self):
+        size = 0
+        node = self.head
+        while node:
+            size += 1
+            node = node.next
+
+        return size
+
+    def to_list(self):
+        out = []
+        current = self.head
+        while current:
+            out.append(current.value)
+            current = current.next
+        return out
+
+def union(llist1, llist2):
+    list1 = llist1.to_list()
+    list2 = llist2.to_list()
+
+    union_list = list(set(list1+list2))
+
+    ulist = LinkedList()
+    for num in union_list:
+        ulist.append(num)
+
+    return ulist
+
+def intersection(llist1, llist2):
+    set1 = set(llist1.to_list())
+    set2 = set(llist2.to_list())
+
+    intersect_list =[num for num in set1 if num in set2]
+
+    intersection_llist = LinkedList()
+    for num in intersect_list:
+        intersection_llist.append(num)
+
+    return intersection_llist
+
+if __name__ == "__main__":
+    linked_list_1 = LinkedList()
+    linked_list_2 = LinkedList()
+
+    element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 21]
+    element_2 = [6, 32, 4, 9, 6, 1, 11, 21, 1]
+
+    for i in element_1:
+        linked_list_1.append(i)
+
+    for i in element_2:
+        linked_list_2.append(i)
+
+    print(union(linked_list_1, linked_list_2))
+    print(intersection(linked_list_1, linked_list_2))
+
+    linked_list_3 = LinkedList()
+    linked_list_4 = LinkedList()
+
+    element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 23]
+    element_2 = [1, 7, 8, 9, 11, 21, 1]
+
+    for i in element_1:
+        linked_list_3.append(i)
+
+    for i in element_2:
+        linked_list_4.append(i)
+
+    print(union(linked_list_3, linked_list_4))
+    print(intersection(linked_list_3, linked_list_4))
