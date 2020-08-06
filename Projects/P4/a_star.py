f72002501631915c7dcf759da9fc9a07cf57d96c
akhopkar01 <aadi0110@gmail.com>
2020-08-06 00:53:43 -0400

    Bugfixes: fixed the update rule

--- Projects/P4/a_star.py
+++ Projects/P4/a_star.py
@@ -19,6 +19,9 @@ class PriorityQueue:
     def pop(self):
         return self.queue.pop(0)
 
+    def is_empty(self):
+        return len(self.queue) == 0
+
     def __repr__(self):
         return " {} ".format(self.queue)
         
@@ -27,54 +30,48 @@ def heuristic(p1, p2):
     return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
 def shortest_path(M, start, target):
-    visited_nodes = set() # Keep track of visited node
-    queue = PriorityQueue() # priority queue
-    
-    # Cost matrix initialization - setting to Infinity
+    queue = PriorityQueue()  # priority queue
+
+    # Cost matrix initialization
     cost_matrix = {}
-    for node in M.intersections:
-        cost_matrix[node] = math.inf
     cost_matrix[start] = 0
-    
-    visited_nodes.add(start)
+
     queue.push(start, cost_matrix[start])
-    
+
     # Path explored initialization
     path = {}
     path[start] = None
     target_found = False
-    
-    while queue and not target_found:
+
+    while not queue.is_empty() and not target_found:
         node = queue.pop()
         current_node = node.node
         node_cost = node.cost
-        
+
         if current_node == target:
             # Target found, break loop
             target_found = True
-        
-        visited_nodes.add(current_node)
-        
+
         # Check all neighbours of the road
         for neighbour in M.roads[current_node]:
-            if neighbour not in visited_nodes:
-                
-                old_cost = cost_matrix[neighbour] 
-                new_cost = node_cost + heuristic(M.intersections[current_node], M.intersections[neighbour]) # cost to come + cost to go
-                
-                if new_cost < old_cost:
-                    cost_matrix[neighbour] = new_cost # Update cost matrix
-                    queue.push(neighbour, new_cost) # push new node to queue
-                    path[neighbour] = current_node # update path explored
-                    
-    
+            cost_to_come = cost_matrix[current_node] + heuristic(M.intersections[current_node],
+                                                                 M.intersections[neighbour])
+            if neighbour not in cost_matrix or cost_to_come < cost_matrix[neighbour]:
+                cost_matrix[neighbour] = cost_to_come  # Update Cost matrix
+                new_cost = cost_to_come + heuristic(M.intersections[neighbour],
+                                                    M.intersections[target])  # cost to come + cost to go
+                queue.push(neighbour, new_cost)  # push new node to queue
+                path[neighbour] = current_node  # update path explored
+
     # Backtrack [goal - start]
-    
-    final_path = []
-    current = target
-    while target is not start:
-        final_path.append(target)
-        target = path[target]
-    final_path.append(start)
-    
-    return final_path[::-1]
+    if target_found == True:
+        final_path = []
+        current = target
+        while target is not start:
+            final_path.append(target)
+            target = path[target]
+        final_path.append(start)
+
+        return final_path[::-1]
+    else:
+        return None
